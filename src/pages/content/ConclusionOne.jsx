e636ed296d7cd35613557ba00e6b9070973df96e
“eddiejoe-antonio” <108960000+eddiejoe-antonio@users.noreply.github.com>
2023-12-14 17:50:06 -0500

    Conclusion content

--- src/pages/content/ConclusionOne.jsx
+++ src/pages/content/ConclusionOne.jsx
@@ -8,13 +8,13 @@ import ButtonLight from '../../components/ButtonLight';
 const ConclusionOne = () => {
     return ( 
         <div className='w-full h-screen px-4 font-sans'>
-            <div className='grid md:grid-cols-12'>
+            <div className='grid md:grid-cols-12 font-sans'>
                 <div className='font-sans flex flex-col md:col-start-4 md:col-span-6 justify-center pt-[10vh]'>
                     <TexasStripes />
                     <p className='py-12'>
                     The Texas Digital Opportunity Plan presents this set of strategies in response to specific digital needs statewide.
                    </p>
-                    <p className='py-12'>
+                    <p>
                     The strategies this plan proposes will ensure that all Texans are able to fully participate in our 21st century economy and social and civil society.     
                     </p>
                 </div>
@@ -22,12 +22,12 @@ const ConclusionOne = () => {
                     <p className='py-12 font-sans'>
                     However, the Texas Digital Opportunity Plan is intended to be a living document. We welcome your comments at this link, your feedback at our email address, and your continued participation in the expansion of digital opportunity in Texas through public events or other forums.
                     </p>
-                    <p className='py-12 font-sans'>
+                    <p>
                     If you want to dive deeper into specific issues, specific counties or regions, or anything else, click here to review the BDO’s Digital Opportunity Dashboards.      
                     </p>
-                    <div className='flex justify-between py-8'> 
-                        <ButtonDark text='Interactive Plan'></ButtonDark>
-                        <ButtonLight text='Data Dashboards'></ButtonLight>
+                    <div className='flex justify-between py-[20vh] w-full'> 
+                        <ButtonDark text='Interactive Plan' className='flex-1'></ButtonDark>
+                        <ButtonLight text='Data Dashboards' className='flex-1'></ButtonLight>
                     </div>
                 </div>
             </div>
